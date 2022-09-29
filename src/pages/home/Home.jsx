aee4386771f9ae6e31296b4790e8a3c00f1ca67e
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-29 23:45:53 +0800

    table component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from './../../components/navbar/Navbar'
 import Widget from './../../components/widget/Widget'
 import Chart from './../../components/chart/Chart'
 import Featured from './../../components/featured/Featured'
+import Table from './../../components/table/Table'
 import './home.scss'
 
 const Home = () => {
@@ -20,6 +21,10 @@ const Home = () => {
         <div className='charts'>
           <Featured />
           <Chart title='Last 6 Months (Revenue)' aspect={2 / 1} />
+        </div>
+        <div className='listContainer'>
+          <div className='listTitle'>Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
